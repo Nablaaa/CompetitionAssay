9ab5f22e20c6dc81f736cf695852cfc97306b811
Nablaaa <Eric_Schmidt_99@gmx.de>
2023-11-07 16:19:02 +0100

    script for denoising and binarization

--- scripts/Denoising_and_Binarization.py
+++ scripts/Denoising_and_Binarization.py
@@ -13,13 +13,12 @@ Later it will be integrated in a workflow (Quantification_transwell.py)
 import os
 from skimage.io import imread, imsave
 from CompetitionAssay.datahandling import GetTranswellData
-from CompetitionAssay.denoising import n2vDenoising
 from CompetitionAssay.binarization import myRFC, RemoveSmallObjects
 
 
 def main():
     # define base directory
-    base_dir = "example_data/"
+    base_dir = "example_data/competitions/"
     files_are_in = "/TW_growth/"
 
     RFC_modelpath_WT = (
@@ -29,11 +28,18 @@ def main():
         "models/RandomForestClassifier_transwell/transwell_denoised_2_categories_Mutant.pkl"
     )
 
-    perform_denoising = False
-    # add the path to the denoised data, if perform_denoising = False
-
     # objects have to be at least 49 objects (e.g. 7x7) large
     min_size = 49  # pixels
+
+    perform_denoising = False
+    if perform_denoising:
+        from CompetitionAssay.denoising import n2vDenoising
+
+        # n2v model directory
+        model_name = "n2v_transwell"
+        model_dir = "models"
+
+        myModel = n2vDenoising(model_name=model_name, model_dir=model_dir)
 
     # get all the files in the directory
     different_competitions_folders = os.listdir(base_dir)
@@ -46,6 +52,10 @@ def main():
         if not os.path.exists(output_dir_binary):
             os.makedirs(output_dir_binary)
 
+        # create output directory if it does not exist
+        if not os.path.exists(output_dir_denoised):
+            os.makedirs(output_dir_denoised)
+
         WT_files, Mutant_files = GetTranswellData(base_dir, competition, files_are_in)
 
         for WT_file, Mutant_file in zip(WT_files, Mutant_files):
@@ -56,15 +66,27 @@ def main():
 
             if perform_denoising:
                 # start here the denoising with a pretrained model
-                # see https://colab.research.google.com/drive/18PiNcg6t73GwjJqFbaYiVLJzSjYzFn2F
-                # pass it, if I want to just load the existing files
-                WT_denoised = n2vDenoising(WT_img, visualize=False)
+                WT_denoised = myModel.predict(WT_img)
+                del WT_img
                 imsave(output_dir_denoised + WT_file, WT_denoised)
 
-                Mutant_denoised = n2vDenoising(Mutant_img, visualize=True)
+                Mutant_denoised = myModel.predict(Mutant_img)
+                del Mutant_img
                 imsave(output_dir_denoised + Mutant_file, Mutant_denoised)
 
             else:
+                assert os.path.exists(
+                    base_dir + competition + files_are_in + "denoised/" + WT_file
+                ), (
+                    "Please add a correct path to the denoised files, your current path is: "
+                    + base_dir
+                    + competition
+                    + files_are_in
+                    + "denoised/"
+                    + WT_file
+                    + " and it does not exist"
+                )
+
                 WT_denoised = imread(base_dir + competition + files_are_in + "denoised/" + WT_file)
                 Mutant_denoised = imread(
                     base_dir + competition + files_are_in + "denoised/" + Mutant_file
@@ -74,23 +96,19 @@ def main():
 
             WT_RFC = myRFC(RFC_modelpath_WT)
             WT_binary = WT_RFC.predict(WT_denoised)
+            del WT_denoised
             WT_without_small_objects = RemoveSmallObjects(WT_binary, min_size=min_size)
+            del WT_binary
             imsave(output_dir_binary + WT_file, WT_without_small_objects)
+            del WT_without_small_objects
 
             mutant_RFC = myRFC(RFC_modelpath_Mutant)
             Mutant_binary = mutant_RFC.predict(Mutant_denoised)
+            del Mutant_denoised
             Mutant_without_small_objects = RemoveSmallObjects(Mutant_binary, min_size=min_size)
+            del Mutant_binary
             imsave(output_dir_binary + Mutant_file, Mutant_without_small_objects)
-
-            # start here the quantification A (competition based on area)
-
-            # start here the quantification B (count biofilms)
-
-            # output is a csv file that contains for each competition the 2 values
-
-        # output is a csv file that contains for each folder the 5 values
-
-    # output is a csv file that contains the outputs of all 7 folders
+            del Mutant_without_small_objects
 
 
 if __name__ == "__main__":
