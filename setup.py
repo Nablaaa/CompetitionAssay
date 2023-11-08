d9a294bf7f0e60589dcf4b4399e180f5a43c8156
Nablaaa <Eric_Schmidt_99@gmx.de>
2023-11-08 14:29:01 +0100

    update version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="competitionassay",
-    version="0.0.2",
+    version="0.0.3",
     author="Eric Schmidt",
     author_email="eric_schmidt_99@gmx.de",
     description="Software for the analysis of the competition assay data.",
