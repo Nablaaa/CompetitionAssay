40a180abf7dd181bb5b5c26f84088888e5b06f42
Nablaaa <Eric_Schmidt_99@gmx.de>
2023-11-10 14:11:39 +0100

    iterate over all competitions

--- scripts/Visualize_RFC_Results.py
+++ scripts/Visualize_RFC_Results.py
@@ -13,37 +13,47 @@ import os
 from skimage.io import imread
 from CompetitionAssay import datahandling, visualization
 
-# define base directory
-base_dir = "example_data/competitions/"
-competition = "competition_2_WTmScarlet_dwspFmNeonGreen/"
-files_are_in = "TW_growth/"
 
-# output directory
-output_dir = base_dir + competition + files_are_in + "RFC_output/"
+def main():
+    # define base directory
+    base_dir = "example_data/competitions/"
+    files_are_in = "TW_growth/"
 
-# create output directory if it does not exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    different_competitions_folders = datahandling.GetCompetitionFolders(base_dir)
 
-WT_files, Mutant_files = datahandling.GetTranswellData(base_dir, competition, files_are_in)
-WT_binary, Mutant_binary = datahandling.GetTranswellData(
-    base_dir, competition, files_are_in + "binary/"
-)
+    for competition in different_competitions_folders:
+        # output directory
+        output_dir = base_dir + competition + files_are_in + "RFC_output/"
 
-# go through all the files
-for WT_file, Mutant_file, WT_binary_file, Mutant_binary_file in zip(
-    WT_files, Mutant_files, WT_binary, Mutant_binary
-):
-    WT_img = imread(base_dir + competition + files_are_in + WT_file)
-    Mutant_img = imread(base_dir + competition + files_are_in + Mutant_file)
+        # create output directory if it does not exist
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
-    WT_binary = imread(base_dir + competition + files_are_in + "binary/" + WT_binary_file)
-    Mutant_binary = imread(base_dir + competition + files_are_in + "binary/" + Mutant_binary_file)
+        WT_files, Mutant_files = datahandling.GetTranswellData(base_dir, competition, files_are_in)
+        WT_binary, Mutant_binary = datahandling.GetTranswellData(
+            base_dir, competition, files_are_in + "binary/"
+        )
 
-    ax, merged_plot = visualization.OverlaySegmentation(
-        WT_img, WT_binary, Mutant_img, Mutant_binary
-    )
+        # go through all the files
+        for WT_file, Mutant_file, WT_binary_file, Mutant_binary_file in zip(
+            WT_files, Mutant_files, WT_binary, Mutant_binary
+        ):
+            WT_img = imread(base_dir + competition + files_are_in + WT_file)
+            Mutant_img = imread(base_dir + competition + files_are_in + Mutant_file)
 
-    repetition_name = WT_file[-12:-4]
+            WT_binary = imread(base_dir + competition + files_are_in + "binary/" + WT_binary_file)
+            Mutant_binary = imread(
+                base_dir + competition + files_are_in + "binary/" + Mutant_binary_file
+            )
 
-    merged_plot.savefig(output_dir + repetition_name + "_visualization.png", dpi=300)
+            ax, merged_plot = visualization.OverlaySegmentation(
+                WT_img, WT_binary, Mutant_img, Mutant_binary
+            )
+
+            repetition_name = WT_file[-12:-4]
+
+            merged_plot.savefig(output_dir + repetition_name + "_visualization.png", dpi=300)
+
+
+if __name__ == "__main__":
+    main()
